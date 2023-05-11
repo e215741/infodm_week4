111023b3b7a753cff23bb8d43b5573838b60a315
Aren Kyoda <e215741@ie.u-ryukyu.ac.jp>
2023-05-11 14:36:44 +0900

    1st commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import datasets
+import regression
+
+
+X,Y = datasets.load_linear_exsample1()
+
+#ver1
+#print(X)
+#print(Y)
+
+model = regression.LinearRegression()
+
+model.x
+
+#ver2
+import importlib
+importlib.reload(regression)
+model = regression.LinearRegression()
+model.fit(X, Y)
+print(model.theta)
